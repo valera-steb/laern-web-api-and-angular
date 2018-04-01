04d0f8d7e11884ccb2e19185d73ccdb38c8bebb8
valeriy_tereshenko <tereshenko.valeriy.v@gmail.com>
2018-04-01 19:48:59 +0300

    initial migrations

---
+++ Demo.Domain/Model/Migrations/20180401152804_Init.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Demo.Domain.Model.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersTasks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Points = table.Column<int>(nullable: false),
+                    TaskId = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersTasks_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersTasks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersTasks_TaskId",
+                table: "UsersTasks",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersTasks_UserId",
+                table: "UsersTasks",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersTasks");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Demo.Domain/Model/Migrations/20180401152914_InitData.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Demo.Domain.Model.Migrations
+{
+    public partial class InitData : Migration
+    {
+        /// <summary>
+        /// Плохой пример добавления данных в базу, но по EF не позволяет пользоваться контекстом в миграциях
+        /// </summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var builder = new DbContextOptionsBuilder<ModelContext>();
+            builder.UseSqlite("Data Source=./DemoContext.db");
+
+
+            using (var _context = new ModelContext(builder.Options))
+            {
+                var task1 = new Task
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Task 1"
+                };
+                var task2 = new Task
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Task 2"
+                };
+
+                var task3 = new Task
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Task 3"
+                };
+
+                _context.Tasks.AddRange(task1, task2, task3);
+                _context.SaveChanges();
+
+
+                var usersTask1_1 = new UsersTask
+                {
+                    Id = Guid.NewGuid(),
+                    Points = 10,
+                    TaskId = task1.Id
+                };
+
+                var usersTask1_2 = new UsersTask
+                {
+                    Id = Guid.NewGuid(),
+                    Points = 20,
+                    TaskId = task2.Id
+                };
+
+                var usersTask2_2 = new UsersTask
+                {
+                    Id = Guid.NewGuid(),
+                    Points = 22,
+                    TaskId = task2.Id
+                };
+
+                var usersTask2_3 = new UsersTask
+                {
+                    Id = Guid.NewGuid(),
+                    Points = 33,
+                    TaskId = task3.Id
+                };
+
+                _context.Users.AddRange(
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        UserName = "User1",
+                        UsersTasks = new List<UsersTask> {usersTask1_1, usersTask1_2}
+                    },
+
+                    new User
+                    {
+                        Id = Guid.NewGuid(),
+                        UserName = "User2",
+                        UsersTasks = new List<UsersTask> {usersTask2_2, usersTask2_3}
+                    }
+                );
+                _context.SaveChanges();
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("TRUNCATE TABLE [Tasks]");
+            migrationBuilder.Sql("TRUNCATE TABLE [Users]");
+            migrationBuilder.Sql("TRUNCATE TABLE [UsersTasks]");
+        }
+    }
+}
